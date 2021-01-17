82872c24a72d0df81241002aa8641b14833f49d1
Adebowale Akinola <adebowalen@yahoo.com>
2021-01-17 16:10:28 +0100

    Add style to converter component.

--- src/components/RomanNumeralsConverter/index.js
+++ src/components/RomanNumeralsConverter/index.js
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-
 import RomanNumerals from '../../lib/RomanNumerals';
+import './styles.css';
 
 const RomanNumeralsConverter = () => {
-  const [result, setResult] = useState();
+  const [result, setResult] = useState('');
 
   const handleChange = input => {
     if (!isNaN(input)) return setResult(RomanNumerals.toRoman(+input));
@@ -11,13 +11,18 @@ const RomanNumeralsConverter = () => {
   };
 
   return (
-    <div className="main">
-      <input
-        data-testid="input"
-        type="text"
-        onChange={e => handleChange(e.target.value)}
-      />
-      <div>{result}</div>
+    <div className="container">
+      <div className="converter">
+        <input
+          data-testid="input"
+          type="text"
+          onChange={e => handleChange(e.target.value)}
+          placeholder="Enter integer or Roman numeral"
+        />
+        <div style={{ color: result.length > 20 ? 'red' : 'green' }}>
+          {result}
+        </div>
+      </div>
     </div>
   );
 };
